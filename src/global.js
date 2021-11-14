cf1e19c2afc117a82d8e095260ab7f8e630cfd7a
silvia0115 <silvia.suarez@eia.edu.co>
2021-11-14 18:30:09 -0500

    Heroku

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 const global = {};
 
- global.url = "http://localhost:5000";
-//global.url = "https://final-calculator.herokuapp.com";
+// global.url = "http://localhost:5000";
+global.url = "https://final-calculator.herokuapp.com";
 module.exports = global;
