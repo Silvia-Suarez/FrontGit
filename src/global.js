8e6fb14106a8e0405c0abdb1f1edccc37c7db245
Kevin <kenvi.009@gmail.com>
2021-11-14 21:24:51 -0500

    change url

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 const global = {};
 
 // global.url = "http://localhost:5000";
-global.url = "https://final-calculator.herokuapp.com";
+global.url = "https://final-calculator-back.herokuapp.com";
 module.exports = global;
